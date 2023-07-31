84814c5f70e9dc1c0dd084b2dc514c604687f7c1
Cameron B <Cameron0942@users.noreply.github.com>
2023-07-31 04:20:45 -0700

    cleaned up comments

--- src/App.jsx
+++ src/App.jsx
@@ -16,28 +16,12 @@ import Dashboard from './pages/dashboard';
 
 function App() {
 
-  // useEffect(() => {
-  //   // Function to make the Axios GET request
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get('http://localhost:8080/'); // Replace 'api/data' with your actual endpoint
-  //       // setData(response.data);
-  //       console.log(response.data)
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-
-  //   fetchData(); // Call the function to trigger the GET request when the component is loaded
-  // }, []);
-
   return (
     <Router>
       <Routes>
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/" element={<Store />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/contact" element={Contact} /> */}
       </Routes>
     </Router>
   )
